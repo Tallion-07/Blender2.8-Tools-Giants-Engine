02f583d600dbe1a654e707f818784f82bebcfc49
Tallion_07 <opptrans@gmail.com>
2019-12-02 22:32:55 +1000

    updated myscript

---
+++ addons/myscript.py
@@ -0,0 +1,12 @@
+import sys
+import os
+import bpy
+
+blend_dir = os.path.dirname(bpy.data.filepath)
+if blend_dir not in sys.path:
+    sys.path.append(blend_dir)
+
+from addons.io_export_i3d import i3d_ui, io_export_i3d
+from importlib import reload
+reload(io_export_i3d)
+i3d_ui.main()
